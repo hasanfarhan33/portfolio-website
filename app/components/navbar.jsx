31aa8932c58e1054ddbe71b66b6c240305ac35d2
hasanfarhan33 <hasanfarhan33@gmail.com>
2023-12-09 19:33:32 +0000

    Styled and animated navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -4,12 +4,21 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav
-      className="flex justify-around  bg-indigo-100 font-bold 
-    text-xl"
+      className="top-0 z-50 bg-indigo-100 font-bold 
+    text-xl sticky"
     >
-      <Link href="/">Home</Link>
-      <Link href="/resume">Resume</Link>
-      <Link href="/drawings">Drawings</Link>
+      <div className="flex justify-around py-3">
+        <Link href="/" className="group decoration-black transition duration-300">Home
+          <span className="block max-w-0 group-hover:max-w-full transition-all duration-250 h-1 bg-black rounded-full"></span>
+        </Link>
+        <Link href="/resume" className="group decoration-black transition duration-300">Resume
+          <span className="block max-w-0 group-hover:max-w-full transition-all duration-250 h-1 bg-black rounded-full"></span>
+        </Link>
+        <Link href="/drawings" className="group decoration-black transition duration-300">Drawings
+          <span className="block max-w-0 group-hover:max-w-full transition-all duration-250 h-1 bg-black rounded-full"></span>
+        </Link>
+      </div>
+      
     </nav>
   );
 }
